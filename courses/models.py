ca26ce637623aa09d1104353d52f9181980003ef
Martins <kydzoster@gmail.com>
2020-08-03 15:38:00 +0100

    added content models for diverse content to the course modules.

--- courses/models.py
+++ courses/models.py
@@ -47,6 +47,40 @@ class Module(models.Model):
 
 class Content(models.Model):
     module = models.ForeignKey(Module, related_name='contents', on_delete=models.CASCADE)
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    # limit_choices argument will limit the ContentType objects
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, limit_choices_to={
+        # model_in field lookup will filter the query to the ContentType objects
+        'model__in':('text','video','image','file')
+    })
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+
+# AThis will define fields which will get included in all child models.
+# there wont be a database table for ItemBase() class, due to being an abstract model
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, related_name='%(class)s_related', on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+# stores content
+class Text(ItemBase):
+    content = models.TextField()
+
+# stores files, e.g. PDF
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+# stores images
+class Image(ItemBase):
+       file = models.FileField(upload_to='images')
+
+# stores videos. through url
+class Video(ItemBase):
+    url = models.URLField()
