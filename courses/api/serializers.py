21e4c499a437e6816563e947334307d6827bc8f1
Martins <kydzoster@gmail.com>
2020-08-17 15:06:03 +0100

    added nested serializers

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,8 +1,26 @@
 from rest_framework import serializers
 from ..models import Subject
+from ..models import Course, Module
 
 
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = ['id', 'title', 'slug']
+
+# This will provide serialization for the Module model.
+class ModuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description']
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    # this will serve as a Moduleserializer for CourseSerializer which will 
+    # serialize multiple objects and it will be only read-only 
+    # and wont be included in any input to create or update objects
+    modules = ModuleSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'slug', 'overview', 'created', 'owner', 'modules']
