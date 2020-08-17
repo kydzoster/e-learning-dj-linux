61abe615db28b07ce7d740394910af2674cfbbbd
Martins <kydzoster@gmail.com>
2020-08-17 15:21:26 +0100

    added REST authentication method

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
-from ..models import Subject
+from ..models import Subject, Course
 from .serializers import SubjectSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authentication import BasicAuthentication
 
 
 class SubjectListView(generics.ListAPIView):
@@ -15,3 +19,17 @@ class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     # serializes objects
     serializer_class = SubjectSerializer
+
+# this will handle student enrolement on course.
+class CourseEnrollView(APIView):
+    # users will be identified by the credentials set in the Authorization header of the HTTP request
+    authentication_classes = (BasicAuthentication,)
+
+    # This method will allow only POST method, no other HTTP method is allowed
+    def post(self, request, pk, format=None):
+        # this retrieves course by pk parameter and raises a 404 if its not found
+        course = get_object_or_404(Course, pk=pk)
+        # add current user to the students many-to-many relationship of the course
+        course.students.add(request.user)
+        # return successful response
+        return Response({'enrolled': True})
