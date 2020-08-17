d152d77a5cd5661dd9b4f6f98506c328d6ad616f
Martins <kydzoster@gmail.com>
2020-08-17 16:25:35 +0100

    course contents

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from ..models import Subject, Course
-from .serializers import SubjectSerializer, CourseSerializer
+from .serializers import SubjectSerializer, CourseSerializer, CourseWithContentsSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -8,6 +8,7 @@ from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from .permissions import IsEnrolled
 
 
 class SubjectListView(generics.ListAPIView):
@@ -53,3 +54,13 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
+    # this will perform action on a single object
+    # only GET is allowed, and only users enrolled on course can access its content
+    @action(detail=True,
+            methods=['get'],
+            serializer_class=CourseWithContentsSerializer,
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated, IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        # returns the course object
+        return self.retrieve(request, *args, **kwargs)
